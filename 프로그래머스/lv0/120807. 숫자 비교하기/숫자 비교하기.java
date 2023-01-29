0fa8b09693b12ca5b0f9827732f1782b72c7cad6
M4T4a <123621785+J0mataa@users.noreply.github.com>
2023-01-29 19:19:07 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.01 ms, Memory: 70.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        
+        if(num1==num2){ //두 수가 같으면 1
+            return 1;
+        }else{ //다르면 -1
+            return -1;
+        }
+        
+    }
+}
